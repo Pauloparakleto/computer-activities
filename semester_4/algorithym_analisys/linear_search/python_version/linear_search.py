fb2162d4b6883e1b1b2c0eaafba017d8511cbad5
Pauloparakleto <user.email>
2024-02-22 19:03:50 -0300

    improve algorithy. Do not scan all the list when the index element is already found

--- semester_4/algorithym_analisys/linear_search/python_version/linear_search.py
+++ semester_4/algorithym_analisys/linear_search/python_version/linear_search.py
@@ -1,5 +1,5 @@
 list = [16, 9, -3, 5, 4, 27, 46, 11, -5]
-k = 2
+k = -5
 n = 0
 index = -1 # let us assume it is the default while the number is not found
 # Take the k value and return its index with linear search.
@@ -7,6 +7,8 @@ while n < len(list):
     print('n is: ' + str(n))
     if list[n] is k:
         index = n
+        print(index)
+        break
     n += 1 # increment by 1
 
 print(index) # return the index anyway
