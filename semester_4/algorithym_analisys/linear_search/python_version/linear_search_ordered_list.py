0e0cd113d3aceb99de7471db36e274c478007d73
Pauloparakleto <user.email>
2024-02-24 10:41:05 -0300

    add invariant loop phrase

--- semester_4/algorithym_analisys/linear_search/python_version/linear_search_ordered_list.py
+++ semester_4/algorithym_analisys/linear_search/python_version/linear_search_ordered_list.py
@@ -3,18 +3,15 @@
 # Let us modify the simpl search with this new intention.
 
 list = [-5, -3, 0, 10, 20, 30, 40, 100, 10000] # an ordered list
-k = 2000 # what we are looking for. This is the worst case. It would walk though all the list until realize the number is not there.
-# This is the reason binary search solves this problem better
+k = 20000 # what we are looking for. This is the worst case. It would walk though all the list until realize the number is not there.
+
 n = 0
 index = -1 # let us assume it is the default while the number is not found
 # Take the k value and return its index with linear search.
-while n < len(list):
+while n < len(list) and k >= list[n]: # this is an invariant loop phrase
     print('n is: ' + str(n))
-    if k < list[n]: # prevent keep moving to the right
-        break
     if list[n] is k:
         index = n
-        print(index)
         break
     n += 1 # increment by 1
 
