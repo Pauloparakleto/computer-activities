875868e64da35e91f3934d34c0e3795de396c5e7
Pauloparakleto <user.email>
2024-02-25 15:57:34 -0300

    back to a basc case

--- semester_4/algorithym_analisys/linear_search/python_version/binary_search.py
+++ semester_4/algorithym_analisys/linear_search/python_version/binary_search.py
@@ -6,30 +6,18 @@
 #
 # Apply the loop invariant phrase concept
 # 
-list = [-5, -3, 0, 10, 20, 30, 40, 100, 10000]
+list = [7]
 length = len(list)
 left = 0
-right = length - 1
-k = 20000
+right = int(length / 2)
+k = 7
 
 result = -1
 
 while left <= right:
-    print('left is: ' + str(left))
-    print('right is: ' + str(right))
-    print('k is: ' + str(k))
-    print('list number is: ' + str(list[right]))
-
     if k is list[right]:
         print(str(k) + ' was found at index ' + str(right))
         result = right
-        break
-    if k < list[right]:
-        print(str(k) + ' is less than ' + str(list[right]))
-
-        right = int(right / 2)
-        print('the new middle right index is: ' + str(right))
-    if k > list[right]:
         break
 
 
